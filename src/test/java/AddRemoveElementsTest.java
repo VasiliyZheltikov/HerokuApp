b0ece5fcef19c432c7469d4e4f68d958f8332cca
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-06-22 14:27:55 +0300

    AddRemoveElementsTest MODIFIED CheckboxesTest MODIFIED DropdownTest DONE Homework IN PROGRESS

--- src/test/java/AddRemoveElementsTest.java
+++ src/test/java/AddRemoveElementsTest.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.List;
 
 public class AddRemoveElementsTest {
 
@@ -11,13 +13,16 @@ public class AddRemoveElementsTest {
     public void AddRemoveElements() {
         WebDriver driver = WebDriverSetup.getWebDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        SoftAssert softAssert = new SoftAssert();
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
-        Assert.assertEquals(driver.findElement(By.cssSelector("#content > h3")).getText(), "Add/Remove Elements");
+        WebElement addButton = driver.findElement(By.cssSelector("#content > div > button"));
+        softAssert.assertEquals(driver.findElement(By.cssSelector("#content > h3")).getText(), "Add/Remove Elements");
         for (int i = 0; i < 2; i++) {
-            driver.findElement(By.cssSelector("#content > div > button")).click();
+            addButton.click();
         }
-        driver.findElement(By.cssSelector("#elements > button:nth-child(2)")).click();
-        Assert.assertTrue(driver.findElements(By.cssSelector("#elements > button:nth-child(2)")).isEmpty());
+        List<WebElement> deleteButtons = driver.findElements(By.xpath("//button[text()='Delete']"));
+        deleteButtons.getLast().click();
+        softAssert.assertEquals(deleteButtons.size(), 1);
         driver.quit();
     }
 }
--- src/test/java/CheckboxesTest.java
+++ src/test/java/CheckboxesTest.java
@@ -1,7 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
@@ -11,17 +13,17 @@ public class CheckboxesTest {
     public void Checkboxes() {
         WebDriver driver = WebDriverSetup.getWebDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        SoftAssert softAssert = new SoftAssert();
         driver.get("https://the-internet.herokuapp.com/checkboxes");
-        Assert.assertFalse(Boolean.parseBoolean(driver.findElement(
-                By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)")).getDomProperty("checked")));
-        driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)")).click();
-        Assert.assertTrue(Boolean.parseBoolean(driver.findElement(
-                By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)")).getDomProperty("checked")));
-        Assert.assertTrue(Boolean.parseBoolean(driver.findElement(
-                By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)")).getDomProperty("checked")));
-        driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)")).click();
-        Assert.assertFalse(Boolean.parseBoolean(driver.findElement(
-                By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)")).getDomProperty("checked")));
+        Assert.assertEquals(driver.findElement(By.cssSelector("#content > div > h3")).getText(), "Checkboxes");
+        WebElement checkbox1 = driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(1)"));
+        WebElement checkbox2 = driver.findElement(By.cssSelector("#checkboxes > input[type=checkbox]:nth-child(3)"));
+        softAssert.assertFalse(Boolean.parseBoolean(checkbox1.getDomProperty("checked")));
+        checkbox1.click();
+        softAssert.assertTrue(Boolean.parseBoolean(checkbox1.getDomProperty("checked")));
+        softAssert.assertTrue(Boolean.parseBoolean(checkbox2.getDomProperty("checked")));
+        checkbox2.click();
+        softAssert.assertFalse(Boolean.parseBoolean(checkbox2.getDomProperty("checked")));
         driver.quit();
     }
 }
