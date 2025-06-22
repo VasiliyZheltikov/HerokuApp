35506a9a8e699704bfb74b4e12cbc1a0a7a9022c
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-06-22 18:21:56 +0300

    NotificationMessagesTest DONE Homework DONE

---
+++ src/test/java/NotificationMessagesTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class NotificationMessagesTest {
+
+    @Test
+    public void NotificationMessages() {
+        WebDriver driver = WebDriverSetup.getWebDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        SoftAssert softAssert = new SoftAssert();
+        driver.get("https://the-internet.herokuapp.com/notification_message_rendered");
+        softAssert.assertEquals(driver.findElement(By.cssSelector("#content > div > h3")).getText(),
+                "Notification Message");
+        driver.findElement(By.cssSelector("#content > div > p > a")).click();
+        softAssert.assertTrue(driver.findElement(By.cssSelector("#flash")).isDisplayed());
+        driver.quit();
+        softAssert.assertAll();
+    }
+}
